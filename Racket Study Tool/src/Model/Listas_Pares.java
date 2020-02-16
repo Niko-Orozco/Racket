60cb7d648e96b688d6d4f72f842236462c2803b4
Napsster <nicolas.orozco@utp.edu.co>
2020-02-16 16:00:43 -0500

    listas_pares new data

--- Racket Study Tool/src/Model/Listas_Pares.java
+++ Racket Study Tool/src/Model/Listas_Pares.java
@@ -10,11 +10,50 @@ package Model;
  * @author Niko
  */
 public class Listas_Pares {
+   
 
     public Listas_Pares() {
+        
     }
     
+    public String Pares1(){
+        String data = "";
+        return data;
+    }
     
+    public String Pares2(){
+        String data = "";
+        return data;
+    }
+    
+    public String Pares3(){
+        String data = "";
+        return data;
+    }
+    
+    public String Pares4(){
+        String data = "";
+        return data;
+    }
+    
+    public String EjePares1(){
+        String data = "";
+        return data;
+    }
    
-    
+    public String EjePares2(){
+        String data = "";
+        return data;
+    }
+   
+    public String EjePares3(){
+        String data = "";
+        return data;
+    }
+   
+    public String EjePares4(){
+        String data = "";
+        return data;
+    }
+   
 }
